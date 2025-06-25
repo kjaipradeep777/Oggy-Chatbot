f8dd9b49093b52c079a63a586d5931645a27cf73
kajipradeep777 <kjaipradeep777@gmail.com>
2025-06-25 22:32:40 +0530

    Integrated OpenAI Chat API in ai_engine.py and updated main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-import uvicorn
-from ai_engine import get_response
+from ai_engine import generate_response  # import
 
 app = FastAPI()
 
@@ -11,10 +10,10 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-def read_root(request: Request):
+async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/chat", response_class=HTMLResponse)
 async def chat(request: Request, user_input: str = Form(...)):
-    reply = get_response(user_input)
-    return templates.TemplateResponse("index.html", {"request": request, "user_input": user_input, "reply": reply})
+    response = generate_response(user_input)
+    return templates.TemplateResponse("index.html", {"request": request, "user_input": user_input, "response": response})
